8fe2190dabab8b6ed604a85697840f721f186a25
Amirouche <amirouche@hypermove.net>
2016-08-27 00:12:46 +0200

    cleanup

--- ajgudb/__init__.py
+++
@@ -1,8 +0,0 @@
-# AjuDB - wiredtiger powered graph database
-# Copyright (C) 2015 Amirouche Boubekki <amirouche@hypermove.net>
-from .ajgudb import AjguDB
-from .ajgudb import Vertex
-from .ajgudb import Edge
-
-
-VERSION = '0.6'
