69b005049bc31cfccf7379112912c7684f16bdb5
Amirouche <amirouche@hypermove.net>
2018-10-28 20:48:06 +0100

    pypi: if python3 fails, try python3

--- examples/pypi/download.sh
+++ examples/pypi/download.sh
@@ -3,7 +3,7 @@ set -xe
 mkdir -p $HOME/pypi/
 mkdir -p $HOME/packages/$1
 cd $HOME/packages/$1
-pipenv run pip install $1
+pipenv run --three pip install $1 || (rm -rf $HOME/.local/share/virtualenvs/$1-* && pipenv run --two pip install $1)
 pipenv run pip freeze > $HOME/pypi/$1.txt
 curl https://pypi.org/pypi/$1/json > $HOME/pypi/$1.json
 rm -rf $HOME/.local/share/virtualenvs/$1-*
