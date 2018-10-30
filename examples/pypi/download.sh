a198592e079b1c5eee725e8a61a96e48ddbbab34
Amirouche <amirouche@hypermove.net>
2018-10-30 19:58:23 +0100

    pypi: only fetch top25k pypi packages (via pypinfo)

--- examples/pypi/download.sh
+++ examples/pypi/download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -xe
-mkdir -p $HOME/pypi/
-mkdir -p $HOME/packages/$1
-cd $HOME/packages/$1
-pipenv run --three pip install $1 || (rm -rf $HOME/.local/share/virtualenvs/$1-* && pipenv run --two pip install $1)
-pipenv run pip freeze > $HOME/pypi/$1.txt
-curl https://pypi.org/pypi/$1/json > $HOME/pypi/$1.json
-rm -rf $HOME/.local/share/virtualenvs/$1-*
-rm -rf $HOME/packages
+mkdir -p $HOME/pypi/$1/
+cd $HOME/pypi/$1/
+touch start.timestamp
+curl --location https://pypi.org/pypi/$1/json > metadata.json
+export PIPENV_VENV_IN_PROJECT=1
+pipenv run --three pip install $1 || (rm -rf .venv && pipenv run --two pip install $1)
+pipenv run pip freeze > requirements.txt
+rm -rf .venv
+touch end.timestamp
--- examples/pypi/main.py
+++ examples/pypi/main.py
@@ -1,19 +1,27 @@
+import json
 import os
 import shlex
 from datetime import datetime
 from subprocess import run
 from subprocess import DEVNULL
-try:
-    import xmlrpclib
-except ImportError:
-    import xmlrpc.client as xmlrpclib
 from concurrent.futures import ThreadPoolExecutor
 
+# try:
+#     import xmlrpclib
+# except ImportError:
+#     import xmlrpc.client as xmlrpclib
 
-client = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
-packages = client.list_packages()
 
-print("total packages: {}".format(len(packages)))
+# client = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
+# packages = client.list_packages()
+
+# print("total packages: {}".format(len(packages)))
+
+with open('pypi.top25k.json') as f:
+    packages = f.read()
+
+packages = json.loads(packages)
+packages = [x['project'] for x in packages['rows']]
 
 
 def process(package):
@@ -24,7 +32,7 @@ def process(package):
             shlex.split("./download.sh {}".format(package)),
             stderr=DEVNULL,
             stdout=DEVNULL,
-            timeout=20,
+            timeout=60,
         )
     except Exception:
         print('timeout {}'.format(package))
@@ -33,8 +41,10 @@ def process(package):
         print('success {} @ {}'.format(package, delta))
 
 
-with ThreadPoolExecutor(max_workers=10) as e:
+with ThreadPoolExecutor(max_workers=6) as e:
     for package in packages:
-        filepath = '/home/none/pypi/' + package + '.txt'
+        if package == 0:  # wtf?!
+            continue
+        filepath = '/home/none/pypi/' + package
         if not os.path.exists(filepath):
             e.submit(process, package)
