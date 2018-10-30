1f7b99842dc41b674732946678e279653bb69d08
Amirouche <amirouche@hypermove.net>
2018-10-30 21:51:13 +0100

    pypi: use pipenv to install and output dependencies

--- examples/pypi/download.sh
+++ examples/pypi/download.sh
@@ -4,8 +4,9 @@ mkdir -p $HOME/pypi/$1/
 cd $HOME/pypi/$1/
 touch start.timestamp
 curl --location https://pypi.org/pypi/$1/json > metadata.json
+export PIP_NO_CACHE_DIR=false
 export PIPENV_VENV_IN_PROJECT=1
-pipenv run --three pip install $1 || (rm -rf .venv && pipenv run --two pip install $1)
-pipenv run pip freeze > requirements.txt
+pipenv install --three $1 || (rm -rf .venv Pipfile* && pipenv install --two $1)
+pipenv graph --json-tree > dependencies.json
 rm -rf .venv
 touch end.timestamp
