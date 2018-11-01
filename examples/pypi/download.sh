57241cc8acfca5a976a7a80db52064e5966376ab
Amirouche <amirouche@hypermove.net>
2018-11-01 12:37:47 +0100

    pypi: downlowd: allow pre-release

--- examples/pypi/download.sh
+++ examples/pypi/download.sh
@@ -7,7 +7,7 @@ touch start.timestamp
 curl --location https://pypi.org/pypi/$1/json > metadata.json
 export PIP_NO_CACHE_DIR=false
 export PIPENV_VENV_IN_PROJECT=1
-pipenv install --three $1 || (rm -rf .venv Pipfile* && pipenv install --two $1)
+pipenv install --pre --three $1 || (rm -rf .venv Pipfile* && pipenv install --pre --two $1)
 pipenv graph --json-tree > dependencies.json
 rm -rf .venv
 touch end.timestamp
