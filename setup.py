918c9fd7dcbb75a43f85135da6c30ec5e05a4fe8
Amirouche <amirouche@hypermove.net>
2019-01-27 12:07:29 +0100

    v0.12 bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='hoply',
-    version='0.11',
+    version='0.12',
     author='Amirouche Boubekki',
     author_email='amirouche@hypermove.net',
     url='https://github.com/amirouche/hoply',
