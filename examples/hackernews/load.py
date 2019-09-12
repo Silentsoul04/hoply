28a8d2890c9614d2634d1dee778a7b927fbf3061
Amirouche <amirouche+dev@hyper.dev>
2019-09-12 13:15:56 +0200

    examples/hackernews/load: do no fetch stories' html

--- examples/hackernews/load.py
+++ examples/hackernews/load.py
@@ -1,31 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import time
-from uuid import uuid4
 
 import hoply
 from hoply.okvs.wiredtiger import WiredTiger
-
-import requests
-
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options, executable_path="./geckodriver")
-
-
-def url2html(url):
-    driver.get(url)
-    time.sleep(1)
-    out = driver.page_source
-    return out
-
-
-def make_uid():
-    return uuid4().hex
 
 
 triplestore = ("subject", "predicate", "object")
@@ -57,19 +35,5 @@ with WiredTiger("wt") as storage:
                         raise NotImplementedError()
                     else:
                         triplestore.add(tr, uid, key, value)
-                # if the item has a url key, fetch the url using qt webkit
-                # because most recent webpages rely on in-browser
-                # rendering we need a real browser to fetch the content of
-                # the page.
-                try:
-                    url = item["url"]
-                except KeyError:
-                    pass
-                else:
-                    # check the page still exists and is not a redirect
-                    response = requests.head(url)
-                    if response.status_code == 200:
-                        html = url2html(url)
-                        triplestore.add(tr, uid, "url/html", html)
         except Exception:
             print("> failed!")
